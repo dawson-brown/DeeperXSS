b999ca1cadf11f95ee7f4d4133dfe9e98e45fda9
Dawson Brown <dawsonstbrown@gmail.com>
2021-11-03 15:21:40 -0400

    changes

--- scrapy/xssed/spiders/xss_urls.py
+++ scrapy/xssed/spiders/xss_urls.py
@@ -2,7 +2,6 @@ import scrapy
 
 class XssUrlsSpider(scrapy.Spider):
     name = 'xss_urls'
-    # allowed_domains = ['www.xssed.com/archive/', 'www.xssed.com/mirror/']
     start_urls = ['http://www.xssed.com/archive/page=1']
 
     def parse(self, response):
@@ -11,12 +10,10 @@ class XssUrlsSpider(scrapy.Spider):
             yield response.follow(mirror, self.parse_url)
 
         next_page = response.xpath('//th[@id="contentpaneOpen"]/table/div/span[@class="activelink"]/following::a[1]/@href').get()
-        print(f'-----------------{next_page}')
         if next_page is not None and next_page != '/':
             yield response.follow(next_page, self.parse)
 
     def parse_url(self, response):
-        # print(f'-------------------HERE------------------')
         url = response.xpath('//table/tr/th/table/tr/th[@id="contentpaneOpen"]/table/div//th/text()')[11].get()
         yield {
             'mirror': response.url.split("/")[-2],
