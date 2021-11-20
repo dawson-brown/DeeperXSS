6b49e41f4710023340020520270249906fa91cb0
Dawson Brown <dawsonstbrown@gmail.com>
2021-11-20 14:21:21 -0500

    changes

--- data/xssed_url_cleanup.py
+++ data/xssed_url_cleanup.py
@@ -15,26 +15,31 @@ class DeepURL:
             self.url = urlparse(url)
 
         self._decodings = [
-            self._de_us_ascii_decode,
-            self._de_utf7_decode,
             self._de_url_decode,
             self._de_html_decode,
-            self._de_dword_decode,
             self._de_hex_decode,
-            self._de_base64_decode
+            self._de_base64_decode,
+            self._de_url_unicode_decode
         ]
     
     
-    def _de_us_ascii_decode(self, query_string : str) -> Tuple[bool, str]:
-        return (False, query_string)
+    # def _de_us_ascii_decode(self, query_string : str) -> Tuple[bool, str]:
+    #     return (False, query_string)
 
 
-    def _de_utf7_decode(self, query_string : str) -> Tuple[bool, str]:
-        return (False, query_string)
-    
+    # def _de_utf7_decode(self, query_string : str) -> Tuple[bool, str]:
+    #     return (False, query_string)
+    def _de_url_unicode_decode(self, query_string : str) -> Tuple[bool, str]:
+        try:
+            try_dec = query_string.replace('%', '\\').encode('latin-1').decode('unicode-escape')
+            if try_dec == query_string:
+                return (False, query_string)        
+            return (True, try_dec)
+        except:
+            return (False, query_string)
 
     def _de_url_decode(self, query_string : str) -> Tuple[bool, str]:
-        try_dec = parse.unquote(query_string)
+        try_dec = parse.unquote_plus(query_string)
         if query_string == try_dec:
             return (False, query_string)
         return (True, try_dec)
@@ -47,8 +52,8 @@ class DeepURL:
         return (True, dec_str)
 
 
-    def _de_dword_decode(self, query_string : str) -> Tuple[bool, str]:
-        return (False, query_string)
+    # def _de_dword_decode(self, query_string : str) -> Tuple[bool, str]:
+    #     return (False, query_string)
 
 
     def _de_hex_decode(self, query_string : str) -> Tuple[bool, str]:
@@ -59,8 +64,8 @@ class DeepURL:
             return (False, query_string)
 
 
-    def _de_octal_decode(self, query_string : str) -> Tuple[bool, str]:
-        return (False, query_string)
+    # def _de_octal_decode(self, query_string : str) -> Tuple[bool, str]:
+    #     return (False, query_string)
 
 
     def _de_base64_decode(self, query_string : str) -> Tuple[bool, str]:
@@ -69,7 +74,7 @@ class DeepURL:
                 .replace('_', '/')
 
         try:
-            dec_str = b64decode(query_string).decode("utf-8")
+            dec_str = b64decode(query_string, True).decode("utf-8")
             return (True, dec_str)
         except:
             return (False, query_string)
@@ -77,66 +82,68 @@ class DeepURL:
 
     def _de_decode(self, query_string : str) -> Tuple[bool, str]:
 
+        if query_string == '':
+            return (False, query_string)
+
         all_decodings = list()
         for decoder in self._decodings:
             all_decodings.append( decoder(query_string) )
 
+        some_decoded = False
         for past, dec_str in all_decodings:
+            some_decoded = some_decoded or past
             if past:
                 (next_past, next_dec_str) = self._de_decode(dec_str)
                 if next_past:
                     return (True, next_dec_str)
 
-        if any(c not in string.printable for c in query_string):
+        # any(c not in string.printable for c in query_string) or 
+        if some_decoded:
             return (False, query_string)
         else:
             return (True, query_string)
 
 
-    def decode_url(self) -> str:
+    def decode_url(self) -> Tuple[bool, str]:
         
-        if self.url.query == '':
-            dec_q, dec_query = (False, None)
-        else:
-            dec_q, dec_query = self._de_decode(self.url.query)
-        
-        if self.url.path == '':
-            dec_p, dec_path = (False, None)
-        else:
-            dec_p, dec_path = self._de_decode(self.url.path)
+        dec_q, dec_query = self._de_decode(self.url.query)   
+        dec_p, dec_path = self._de_decode(self.url.path)
+        deq_pa, dec_params = self._de_decode(self.url.params)
+        deq_f, dec_frag = self._de_decode(self.url.fragment)
 
-        if self.url.params == '':
-            deq_pa, dec_params = (False, None)
-        else:
-            deq_pa, dec_params = self._de_decode(self.url.params)
-
-
-        if self.url.params == '':
-            deq_f, dec_frag = (False, None)
-        else:
-            deq_f, dec_frag = self._de_decode(self.url.fragment)
-#<scheme>://<netloc>/<path>;<params>?<query>#<fragment>
         tmp_url = urlunparse([self.url.scheme, 
-            self.url.netloc,
+            'website',
             dec_path,
             dec_params,
             dec_query,
             dec_frag])
+        tmp_url = tmp_url.replace('\n', ' ').replace('\r', '')
 
         self.url = urlparse(tmp_url)
-        return tmp_url
+        return ( dec_q or dec_p or deq_pa or deq_f,  \
+            tmp_url )
 
 
-with open('xss_urls.txt', 'w') as outfile, open('xss_urls_tmp.json', 'r') as infile:
+with open('dec_xss_urls.txt', 'w') as dec_outfile,\
+    open('n_dec_xss_urls.txt', 'w') as n_dec_outfile,\
+    open('xss_urls.json', 'r') as infile:
     lines = infile.read().splitlines()
-    for line in lines:
+    decode_strings = set()
+
+    for i, line in enumerate(lines):
         clean_line = line[9:]
         clean_line = clean_line[:-3]
 
         url = DeepURL(url=clean_line)
-        decoded = url.decode_url()
+        decoded, decoded_str = url.decode_url()
 
-        # print(f'{decoded}')
-        outfile.write(decoded)
-        outfile.write('\n')
+        if decoded:
+            decode_strings.add(decoded_str)
+        else:
+            n_dec_outfile.write(clean_line)
+            n_dec_outfile.write('\n')
+
+    for dec_str in decode_strings:
+        dec_outfile.write(dec_str)
+        dec_outfile.write('\n')
 
