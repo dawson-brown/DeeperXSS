9bb7f50ed223801c25d233dad87a224c4fa5a9c1
matt-r-bush <51662879+matt-r-bush@users.noreply.github.com>
2021-11-29 12:43:45 -0500

    adds incomplete CBOW
    adds copy pasted code from a CBOW tutorial

--- lstm/lstm.py
+++ lstm/lstm.py
@@ -1,4 +1,22 @@
 import tensorflow as tf
+
+## for CBOW
+
+from keras.preprocessing import text
+from keras.utils import np_utils
+from keras.preprocessing import sequence
+
+import keras.backend as K
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, Lambda
+
+# visualize CBOW model structure
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+
+## helper libraries
+
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -19,9 +37,112 @@ test_labels = []
 
 ## tokenization
 
-## Word2vec
 
-## Deep Learning Model
+
+## Word2vec using CBOW ------------------------------------------------------------------------------------------------
+## used for reference: https://www.kdnuggets.com/2018/04/implementing-deep-learning-methods-feature-engineering-text-data-cbow.html
+
+## Build corpus vocabulary
+
+tokenizer = text.Tokenizer()
+tokenizer.fit_on_texts(norm_bible)
+word2id = tokenizer.word_index
+
+# build vocabulary of unique words
+word2id['PAD'] = 0
+id2word = {v:k for k, v in word2id.items()}
+wids = [[word2id[w] for w in text.text_to_word_sequence(doc)] for doc in norm_bible]
+
+vocab_size = len(word2id)
+embed_size = 100
+window_size = 2 # context window size
+
+print('Vocabulary Size:', vocab_size)
+print('Vocabulary Sample:', list(word2id.items())[:10])
+
+## Build a CBOW (context, target) generator
+
+def generate_context_word_pairs(corpus, window_size, vocab_size):
+    context_length = window_size*2
+    for words in corpus:
+        sentence_length = len(words)
+        for index, word in enumerate(words):
+            context_words = []
+            label_word   = []            
+            start = index - window_size
+            end = index + window_size + 1
+            
+            context_words.append([words[i] 
+                                 for i in range(start, end) 
+                                 if 0 <= i < sentence_length 
+                                 and i != index])
+            label_word.append(word)
+
+            x = sequence.pad_sequences(context_words, maxlen=context_length)
+            y = np_utils.to_categorical(label_word, vocab_size)
+            yield (x, y)
+            
+            
+# Test this out for some samples
+i = 0
+for x, y in generate_context_word_pairs(corpus=wids, window_size=window_size, vocab_size=vocab_size):
+    if 0 not in x[0]:
+        print('Context (X):', [id2word[w] for w in x[0]], '-> Target (Y):', id2word[np.argwhere(y[0])[0][0]])
+    
+        if i == 10:
+            break
+        i += 1
+
+
+## build the CBOW model architecture
+
+cbow = Sequential()
+cbow.add(Embedding(input_dim=vocab_size, output_dim=embed_size, input_length=window_size*2))
+cbow.add(Lambda(lambda x: K.mean(x, axis=1), output_shape=(embed_size,)))
+cbow.add(Dense(vocab_size, activation='softmax'))
+cbow.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+
+# view model summary
+print(cbow.summary())
+
+SVG(model_to_dot(cbow, show_shapes=True, show_layer_names=False, 
+                 rankdir='TB').create(prog='dot', format='svg'))
+
+## train the model
+
+for epoch in range(1, 6):
+    loss = 0.
+    i = 0
+    for x, y in generate_context_word_pairs(corpus=wids, window_size=window_size, vocab_size=vocab_size):
+        i += 1
+        loss += cbow.train_on_batch(x, y)
+        if i % 100000 == 0:
+            print('Processed {} (context, word) pairs'.format(i))
+
+    print('Epoch:', epoch, '\tLoss:', loss)
+    print()
+
+## get the word embeddings
+
+weights = cbow.get_weights()[0]
+weights = weights[1:]
+print(weights.shape)
+
+pd.DataFrame(weights, index=list(id2word.values())[1:]).head()
+
+from sklearn.metrics.pairwise import euclidean_distances
+
+# compute pairwise distance matrix
+distance_matrix = euclidean_distances(weights)
+print(distance_matrix.shape)
+
+# view contextually similar words
+similar_words = {search_term: [id2word[idx] for idx in distance_matrix[word2id[search_term]-1].argsort()[1:6]+1] 
+                   for search_term in ['god', 'jesus', 'noah', 'egypt', 'john', 'gospel', 'moses','famine']}
+
+similar_words
+
+## Deep Learning Model ------------------------------------------------------------------------------------------------
 
 model = tf.keras.Sequential([
 
