6267d909edf30f8b22afccbc5348ba25a07860bc
Dawson Brown <dawsonstbrown@gmail.com>
2021-12-02 12:24:46 -0500

    changes

--- xss_filters/data/tokenizer.py
+++ xss_filters/data/tokenizer.py
@@ -3,6 +3,7 @@ from urllib.parse import urlparse
 from typing import Any, List, Tuple
 import re
 from random import choice
+from random import randrange
 from pickle import dump
 from sys import argv
 
@@ -13,7 +14,7 @@ tokens = {
     'start_label': re.compile('<[^>\s/]+(>|\s)'),
     'end_label': re.compile('</[^>/]+>'),
     'events': re.compile('on[a-zA-Z]+='),
-    'identifier': re.compile('(?:[a-zA-Z$_][a-zA-Z$_0-9]*\.)*[a-zA-Z$_][a-zA-Z$_0-9]*'),
+    'identifier': re.compile('[a-zA-Z$_][a-zA-Z$_0-9]*'),
     # 'string_arg' : re.compile('\s*(\"(?:(\\(\\|n|r|\')|[^\\\n\r\"])*\"|\'(?:(\\(\\|n|r|\")|[^\\\n\r\'])*\')\s*\)'),
     'int_constant': re.compile('[0-9]+\.?[0-9]*'),
     'other': re.compile('\{\[|\]|\.|\;|\,|<|>|=|!|\+|-|\*|%|&|\||\^|~|\?|:|/|\'|"')
@@ -83,7 +84,7 @@ def tokenize(url: str) -> List[JSToken]:
                             tokens_to_append.append(JSToken('str_arg', '("str_arg")', matched.end(), arg_end+1))
                             # token_list.append( JSToken('str_arg', '("str_arg")', matched.end(), arg_end+1) )
                 else:
-                    tokens_to_append = [JSToken(token_type, matched.group(0).lower(), matched.start(), matched.end())]
+                    tokens_to_append = [JSToken(token_type, 'identifier', matched.start(), matched.end())]
                     # token_list.append( ( JSToken(token_type, matched.group(0), matched.start(), matched.end()) ) )
             elif token_type == 'int_constant':
                 tokens_to_append = [JSToken(token_type, '1', matched.start(), matched.end())]
@@ -105,6 +106,21 @@ def print_token_list(url_tok: URLTokens):
         print(token)
     print('')
 
+def tokenize_to_std(in_name: str, num = 0):
+
+    with open(in_name, 'r') as infile:
+        lines = infile.read().splitlines()
+
+        start = randrange(len(lines))
+        step = randrange(100)
+        print(f'{start} - {step}')
+
+        for i in range(num):
+            url = lines[start]
+            start = (start + step) % len(lines)
+            tmp = URLTokens(url, tokenize(url))
+            print_token_list(tmp)
+
 def tokenize_to_file(in_name: str, start_line = 0, end_line = -1):
 
     out_name = f'{in_name}_{start_line}-{end_line}.dat'
@@ -119,7 +135,6 @@ def tokenize_to_file(in_name: str, start_line = 0, end_line = -1):
             if i%100 == 0:
                 print(f'\tLine: {i}')
             tmp = URLTokens(url, tokenize(url))
-            # print_token_list(tmp)
             dump(tmp, outfile)
 
 if __name__ == "__main__":
@@ -142,8 +157,8 @@ if __name__ == "__main__":
         if len(argv) > 2:
             end_i = argv[2]
 
-    print(f'XSSed...{start_i} - {end_i}')
-    tokenize_to_file('dmoz_dir.txt', int(start_i), int(end_i))
+    tokenize_to_file('dec_xss_urls.txt', int(start_i), int(end_i))
+    # tokenize_to_std('dec_xss_urls.txt', int(start_i))
 
     # url = 'http://website/Hotel-Search?action=hotelSearchWizard@searchHotelOnly&hotelSearchWizard_inpItid=&hotelSearchWizard_inpItty=&hotelSearchWizard_inpItdx=&hotelSearchWizard_inpSearchMethod=usertyped&hotelSearchWizard_inpSearchKeywordIndex=&hotelSearchWizard_inpSearchKeyword=&hotelSearchWizard_inpSearchRegionId=&hotelSearchWizard_inpSearchLatitude=&hotelSearchWizard_inpSearchLongitude=&hotelSearchWizard_inpSearchNear=/"><script>alert(\'Xss ByAtm0n3r\')</script>&hotelSearchWizard_inpSearchNearType=CITY&hotelSearchWizard_inpSearchNearStreetAddr=&hotelSearchWizard_inpSearchNearCity=&hotelSearchWizard_inpSearchNearState=&hotelSearchWizard_inpSearchNearZipCode=&hotelSearchWizard_inpCheckIn=jj/mm/aa&hotelSearchWizard_inpCheckOut=jj/mm/aa&hotelSearchWizard_inpNumRooms=1&hotelSearchWizard_inpNumAdultsInRoom=1&hotelSearchWizard_inpNumChildrenInRoom=0&hotelSearchWizard_inpAddOptionFlag=&hotelSearchWizard_inpHotelName=&hotelSearchWizard_inpHotelClass=0&searchWizard_wizardType=hotelOnly'
     # tmp = URLTokens(url, tokenize(url))
