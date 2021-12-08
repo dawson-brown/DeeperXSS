7935a12ffe4787bbe8e6f20f486dedd6e37e3fed
matt-r-bush <51662879+matt-r-bush@users.noreply.github.com>
2021-12-08 10:35:55 -0500

    precision, recall, fi
    fills out helper functions to get precision, recall and accuracy

--- xss_filters/metrics.py
+++ xss_filters/metrics.py
@@ -1,12 +1,24 @@
 import numpy as np
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 
-def precision(predicted_labels: np.array, actual_labels: np.array) -> None:
-    pass
+def get_precision(predicted_labels: np.array, actual_labels: np.array):
+    precision = precision_score(actual_labels, predicted_labels)
+    print("Precision is: ", precision)
+    return precision
 
 
-def recall(predicted_labels: np.array, actual_labels: np.array) -> None:
-    pass
+def get_recall(predicted_labels: np.array, actual_labels: np.array):
+    recall = recall_score(actual_labels, predicted_labels)
+    print("Recall is: ", recall)
+    return recall
 
 
-def f1(predicted_labels: np.array, actual_labels: np.array) -> None:
-    pass
+def get_f1(predicted_labels: np.array, actual_labels: np.array):
+    f1 = f1_score(actual_labels, predicted_labels)
+    print("F1 is: ", f1)
+    return f1
+
+def get_accuracy(predicted_labels: np.array, actual_labels: np.array):
+    accuracy = accuracy_score(actual_labels, predicted_labels)
+    print("Accuracy is: ", accuracy)
+    return accuracy
